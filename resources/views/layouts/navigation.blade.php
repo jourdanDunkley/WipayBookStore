60116619732a6eb9f8aee20d03f5aea3ca6aa275
Jourdan <dunkley.jourdan@gmail.com>
2022-03-07 16:59:05 -0500

    updated responsive menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -92,20 +92,32 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    {{-- <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            @if(Auth::check())
+                @if(Auth::user()->is_admin)
+            
+                <x-responsive-nav-link :href="route('inventory')" :active="request()->routeIs('inventory')">
+                    {{ __('Inventory') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('sales')" :active="request()->routeIs('sales')">
+                    {{ __('Sales') }}
+                </x-responsive-nav-link>
+                
+                @endif
+            @endif
         </div>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
+            @if (Auth::check())
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
             </div>
-
             <div class="mt-3 space-y-1">
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
@@ -118,6 +130,33 @@
                     </x-responsive-nav-link>
                 </form>
             </div>
+                
+            @else
+            <div class="mt-3 space-y-1">
+                <!-- Authentication -->
+                <form method="GET" action="{{ route('login') }}">
+                    @csrf
+
+                    <x-responsive-nav-link :href="route('login')"
+                            onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                        {{ __('Log In') }}
+                    </x-responsive-nav-link>
+                </form>
+            </div>
+            <div class="mt-3 space-y-1">
+                <!-- Authentication -->
+                <form method="GET" action="{{ route('register') }}">
+                    @csrf
+
+                    <x-responsive-nav-link :href="route('register')"
+                            onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                        {{ __('Register') }}
+                    </x-responsive-nav-link>
+                </form>
+            </div>
+            @endif
         </div>
-    </div> --}}
+    </div>
 </nav>
